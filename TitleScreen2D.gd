df3cb65a68e62483470bfadc4f11d3a28b8c5180
richard <github@fantastic.me.uk>
2021-07-03 10:11:17 +0100

    remove all the things

--- TitleScreen2D.gd
+++ TitleScreen2D.gd
@@ -1,19 +1,7 @@
 extends Control
 
-
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-
-
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	if Input.is_action_just_pressed("fire") or Input.is_action_just_pressed("VR_RIGHT_INDEX_TRIGGER") or Input.is_action_just_pressed("VR_LEFT_INDEX_TRIGGER") or Input.is_action_just_pressed("VR_SCREEN_TAP"):
-		get_tree().change_scene("res://FirstPerson.tscn")
+		get_tree().change_scene("res://Game.tscn")
 	elif Input.is_key_pressed(KEY_A):
 		get_tree().change_scene("res://FirstPersonPhysics.tscn")
